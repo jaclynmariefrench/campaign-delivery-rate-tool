8226a2e33921d51a443326bdaa22432c702b75d6
Jackie French <jaclynmariefrench@gmail.com>
2024-07-09 12:22:31 -0400

    added inline-css styling

--- server/adminjs/ratingRuleForm.jsx
+++ server/adminjs/ratingRuleForm.jsx
@@ -1,8 +1,17 @@
 import React, { useState } from "react";
-import { Box, Label, Input, Button, Select } from "@adminjs/design-system";
+import {
+  Box,
+  Label,
+  Input,
+  Button,
+  Select,
+  Header,
+  Section,
+} from "@adminjs/design-system";
+
 
 // just some regular React component
-const RatingRuleForm = () => {
+function RatingRuleForm() {
   const [formData, setFormData] = useState({
     name: "",
     condition: "",
@@ -70,54 +79,80 @@ const RatingRuleForm = () => {
     return getOptions(name).find((option) => option.value === value) || null;
   };
 
+  const inputContainer = {
+    marginBottom: '2.5rem',
+  }
+
+  const buttonContainer = {
+    display: 'flex',
+    justifyContent: 'space-around',
+    padding: '0px 32px 32px',
+    border: 'none',
+  }
+
   return (
-    <Box as="form" onSubmit={handleSubmit}>
-      <Label htmlFor="name">Name</Label>
-      <Select
-        id="name"
-        name="name"
-        value={getOption("name", formData.name)}
-        onChange={handleSelectChange("name")}
-        options={getOptions("name")}
-      />
-      <Label htmlFor="condition">Condition</Label>
-      <Select
-        id="condition"
-        name="condition"
-        value={getOption("condition", formData.condition)}
-        onChange={handleSelectChange("condition")}
-        options={getOptions("condition")}
-      />
-
-      <Label htmlFor="minValue">Min Value</Label>
-      <Input
-        id="minValue"
-        name="minValue"
-        value={formData.minValue}
-        onChange={handleInputChange}
-      />
-      {showMaxValue && (
-        <>
-          <Label htmlFor="maxValue">Max Value</Label>
-          <Input
-            id="maxValue"
-            name="maxValue"
-            value={formData.maxValue}
-            onChange={handleInputChange}
-          />
-        </>
-      )}
-
-      <Label htmlFor="score">Score</Label>
-      <Input
-        id="score"
-        name="score"
-        value={formData.score}
-        onChange={handleInputChange}
-      />
-      <Button type="submit">Submit</Button>
-    </Box>
+    <Section>
+      <Box variant="gray">
+        <Header.H2>Some header</Header.H2>
+      </Box>
+      <Box as="form" onSubmit={handleSubmit}>
+        <Box variant="white">
+          <Box style={inputContainer}>
+            <Label htmlFor="name">Name</Label>
+            <Select
+              id="name"
+              name="name"
+              value={getOption("name", formData.name)}
+              onChange={handleSelectChange("name")}
+              options={getOptions("name")} />
+          </Box>
+          <Box style={inputContainer}>
+            <Label htmlFor="condition">Condition</Label>
+            <Select
+              id="condition"
+              name="condition"
+              value={getOption("condition", formData.condition)}
+              onChange={handleSelectChange("condition")}
+              options={getOptions("condition")} />
+          </Box>
+          <Box style={inputContainer}>
+            <Label htmlFor="minValue">Min Value</Label>
+            <Input
+              id="minValue"
+              name="minValue"
+              value={formData.minValue}
+              onChange={handleInputChange}
+              width={1} />
+            {showMaxValue && (
+              <>
+                <Label htmlFor="maxValue">Max Value</Label>
+                <Input
+                  id="maxValue"
+                  name="maxValue"
+                  value={formData.maxValue}
+                  onChange={handleInputChange}
+                  width={1} />
+              </>
+            )}
+          </Box>
+          <Box style={inputContainer}>
+            <Label htmlFor="score">Score</Label>
+            <Input
+              id="score"
+              name="score"
+              value={formData.score}
+              onChange={handleInputChange}
+              width={1} />
+          </Box>
+          <Section style={buttonContainer}>
+            <Button variant="contained" type="submit">
+              Submit
+            </Button>
+          </Section>
+        </Box>
+      </Box>
+    </Section>
   );
-};
+}
 
 export default RatingRuleForm;
