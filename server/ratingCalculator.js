09f8033a70f7aa26f407ff51e0c7458a9749f27b
Jackie French <jaclynmariefrench@gmail.com>
2024-07-25 16:51:54 -0500

    edited ratingcal to be dynamic

---
+++ server/ratingCalculator.js
@@ -0,0 +1,70 @@
+import RatingRule from "./models/ratingSchema.js";
+import DeliverabilityRating from "./models/deliverabilityRatingSchema.js";
+
+
+function evaluateCondition(rate, condition, minValue, maxValue) {
+  switch (condition) {
+    case ">=":
+      return rate >= minValue;
+    case "Between":
+      return rate >= minValue && rate <= maxValue;
+    case ">":
+      return rate > minValue;
+    case "<":
+      return rate < minValue;
+    case "<=":
+      return rate <= minValue;
+    default:
+      return false;
+  }
+}
+
+export async function calculateDeliverabilityRating(
+  deliveryRate,
+  openRate,
+  clickRate,
+  unsubscribeRate,
+  complaintRate
+) {
+  let score = 0;
+
+  // Fetch the rules from the database
+  const rules = await RatingRule.find();
+
+  // Deliverability rating table rules
+  const ratingRules = await DeliverabilityRating.find();
+
+  // Validate and parse the rates
+  const rates = {
+    "Delivery Rate": parseFloat(deliveryRate),
+    "Open Rate": parseFloat(openRate),
+    "Click Rate": parseFloat(clickRate),
+    "Unsubscribe Rate": parseFloat(unsubscribeRate),
+    "Complaint Rate": parseFloat(complaintRate),
+  };
+
+
+  // Calculate the score based on the rules
+  rules.forEach(rule => {
+    const rate = rates[rule.name];
+    if (evaluateCondition(rate, rule.condition, rule.minValue, rule.maxValue)) {
+      score += rule.score;
+    }
+  });
+
+
+  // Determine the deliverability rating based on the score
+  let deliverabilityRating = "Undefined";
+  ratingRules.forEach(ratingRule => {
+    if (evaluateCondition(score, ratingRule.condition, ratingRule.minScore, ratingRule.maxScore)) {
+      deliverabilityRating = ratingRule.ratingTerm;
+    }
+  });
+
+
+  // Return the calculated score and deliverability rating
+  return {
+    score,
+    deliverabilityRating
+  };
+}
